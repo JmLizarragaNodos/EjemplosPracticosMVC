a8a8c689c00db484ae5df20dd36eb828a96dc645
JmLizarragaNodos <jlizarraga@nodos.cl>
2023-11-03 13:42:41 -0300

    Ejemplo Select2

---
+++ Web/Controllers/Select2Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Web.Dto;
+
+namespace Web.Controllers
+{
+    public class Select2Controller : Controller
+    {
+        private static List<Probando> datos;
+
+        static Select2Controller()
+        {
+            // Así, la inicialización de datos se realiza solo una sola vez durante el ciclo de vida de la aplicación
+            datos = new List<Probando>();
+
+            Enumerable.Range(1, 15).ToList().ForEach(numero => {
+                datos.Add(new Probando { codigo = numero, nombre = $"Nombre {numero}" });
+            });
+        }
+
+        public Select2Controller()
+        {
+           
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public class Probando
+        {
+            public int codigo { get; set; }
+            public string nombre { get; set; }
+        }
+
+        [HttpPost]
+        public ActionResult ObtenerDatosSelect2(string search, int pageSize, int page)
+        {
+            int offset = (page - 1) * pageSize;       // Cálculo del offset
+            var dt = datos.Skip(offset).Take(pageSize).ToList();
+
+            int cantidadDatosCursor = dt.Count;
+            bool traerMasRegistros = (cantidadDatosCursor > 0 && cantidadDatosCursor == pageSize);
+            var retorno = new Select2(traerMasRegistros);
+
+            foreach (Probando x in dt)
+            {
+                retorno.AgregarOption(x.codigo.ToString(), x.nombre);
+            }
+
+            return Json(retorno);
+        }
+    }
+}
---
+++ Web/Dto/Select2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Web.Dto
+{
+    public class Select2
+    {
+        public List<OptionSelect2> resultados { get; set; }
+        public bool traerMasRegistros { get; set; }
+        public object informacionExtra { get; set; }
+
+        public Select2(bool traerMasRegistros)
+        {
+            resultados = new List<OptionSelect2>();
+            this.traerMasRegistros = traerMasRegistros;
+        }
+
+        public void AgregarOption(string id, string text)
+        {
+            resultados.Add(new OptionSelect2(id, text));
+        }
+    }
+
+    public class OptionSelect2
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+
+        public OptionSelect2(string id, string text)
+        {
+            this.id = id;
+            this.text = text;
+        }
+    }
+}
