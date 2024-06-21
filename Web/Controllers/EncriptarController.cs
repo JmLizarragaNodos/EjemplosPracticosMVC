ddb341db93b3c1d65ea7712c2adbae49ebf43717
JmLizarragaNodos <jlizarraga@nodos.cl>
2024-06-21 11:16:19 -0400

    Encriptar y desencriptar texto

---
+++ Web/Controllers/EncriptarController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web.Mvc;
+using Web.Dto;
+using Web.Helpers;
+
+namespace Web.Controllers
+{
+    public class EncriptarController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EncriptarTexto(string texto)
+        {
+            var res = new RespuestaBackend();
+
+            try
+            {
+                var textoEncriptado = CryptoHelper.Encriptar(texto);
+                res.objeto = new { textoEncriptado };
+            }
+            catch (Exception ex)
+            {
+                res.AgregarInternalServerError(ex.Message);
+            }
+
+            return Json(res);
+        }
+
+        [HttpPost]
+        public ActionResult DesencriptarTexto(string textoEncriptado)
+        {
+            var res = new RespuestaBackend();
+
+            try
+            {
+                bool estaEncriptado = CryptoHelper.TryDesencriptar(textoEncriptado, out var textoDesencriptado);   // Validar si está encriptado
+
+                if (estaEncriptado)
+                    res.objeto = new { textoDesencriptado };
+                else
+                    res.AgregarBadRequest("texto no tiene el formato correcto");
+            }
+            catch (Exception ex)
+            {
+                res.AgregarInternalServerError(ex.Message);
+            }
+
+            return Json(res);
+        }
+    }
+}
